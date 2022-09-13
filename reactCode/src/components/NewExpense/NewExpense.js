ad1f6a7891a0ce91d5f443c402df87a890582f42
biyamn <b1y9mn@gmail.com>
2022-09-13 18:07:35 +0900

    데이터 끌어올리기

--- reactCode/src/components/NewExpense/NewExpense.js
+++ reactCode/src/components/NewExpense/NewExpense.js
@@ -4,14 +4,14 @@ import React from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
   const onSaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
-    console.log(expenseData)
-  } 
+    props.onAddExpense(expenseData);
+  };
   return <div className='new-expense'>
     <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}/>
   </div>
