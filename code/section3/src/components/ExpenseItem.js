0e92544dd1e3efb647fda140e61dadefe1f3e4fd
HamZik <b1y9mn@gmail.com>
2023-01-21 22:10:38 +0900

    docs: 섹션 3 - 4 studylog 업로드

--- code/section3/src/components/ExpenseItem.js
+++ code/section3/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 0, 21);
+  const expenseTitle = '붕어빵';
+  const expenseAmount = 3_000;
   return (
     <div className='expense-item'>
-      <div>2023년 1월 21일</div>
+      <div>{expenseDate.toLocaleString()}</div>
       <div className='expense-item__description'>
-        <h2>붕어빵</h2>
-        <div className='expense-item__price'>3,000 원</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>₩{expenseAmount.toLocaleString()}</div>
       </div>
     </div>
   )
