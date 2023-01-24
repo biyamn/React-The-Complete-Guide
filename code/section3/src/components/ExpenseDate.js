bdf45c5e1dd1adecc9b11f3c6c61dcaea1be6fe8
HamZik <b1y9mn@gmail.com>
2023-01-24 16:16:01 +0900

    feat: ExpenseDate 컴포넌트 분리

---
+++ code/section3/src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import './ExpenseDate.css';
+
+function ExpenseDate(props) {
+  const year = props.date.toLocaleString("ko-KR",  {year: "numeric"}); // 2023년
+  const month = props.date.toLocaleString("ko-KR", {month: "long"}); // 1월
+  const day = props.date.toLocaleString("ko-KR", {day: "2-digit"}); // 01일
+
+  return (
+    <div className='expense-date'>
+      <div className='expense-date__month'>{year}</div>  
+      <div className='expense-date__year'>{month}</div>  
+      <div className='expense-date__day'>{day}</div>
+    </div>
+    )
+}
+
+export default ExpenseDate;
