cec3632aa249227a1adc6fede36fecf961e14e6f
b1y9mn <b1y9mn@gmail.com>
2023-01-25 14:53:29 +0900

    feat: Card 컴포넌트 추가

---
+++ code/section3/src/components/Card.js
@@ -0,0 +1,12 @@
+import './Card.css';
+
+function Card(props) {
+
+  return (
+    <div>
+
+    </div>
+    )
+}
+
+export default Card;
--- code/section3/src/components/ExpenseItem.js
+++ code/section3/src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import React from "react";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
@@ -12,7 +12,7 @@ function ExpenseItem(props) {
           ₩{props.amount.toLocaleString()}
         </div>
       </div>
-    </div>
+    </Card>
   );
 }
 
