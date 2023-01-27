94d20fecfd7eb7c187d0040fe1d3ac2baf1e4e3b
b1y9mn <b1y9mn@gmail.com>
2023-01-27 22:56:08 +0900

    feat: clickHandler 추가

--- code/section4/section4-code/src/UI/Card.js
+++ code/section4/section4-code/src/UI/Card.js
@@ -2,7 +2,6 @@ import './Card.css';
 
 const Card = (props) => {
   const classes = 'card ' + props.className;
-  console.log('Card props: ', props);
   return (
     <div className={classes}>
       {props.children}
