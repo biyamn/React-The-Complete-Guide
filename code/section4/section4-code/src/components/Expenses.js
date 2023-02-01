f6f3e9848f4b666926f2bbc73bde6aefc2dfffef
b1y9mn <b1y9mn@gmail.com>
2023-02-01 21:49:01 +0900

    feat: 과제 작성 후 업로드

--- code/section4/section4-code/src/components/Expenses.js
+++ code/section4/section4-code/src/components/Expenses.js
@@ -2,13 +2,20 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
-
+import { useState } from 'react';
 
 const Expenses = (props) => {
+  const [year, setYear] = useState('');
 
+  const saveYearHandler = (selectedYear) => {
+    setYear(selectedYear);
+  }
+
+  console.log(year);
+  
   return (
     <div>
-      <ExpensesFilter />
+      <ExpensesFilter onSaveYearHandler={saveYearHandler} />
       <Card className='expenses'>
         <ExpenseItem 
           title={props.expenses[0].title} 
--- code/section4/section4-code/src/components/ExpensesFilter.js
+++ code/section4/section4-code/src/components/ExpensesFilter.js
@@ -1,13 +1,18 @@
-import React from 'react';
-
 import './ExpensesFilter.css';
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+  let selectedYear;
+
+  const handleChangeSelect = (e) => {
+    selectedYear = e.target.value;
+    props.onSaveYearHandler(selectedYear);
+  }
+
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select onChange={handleChangeSelect}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
