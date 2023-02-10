ef1d8f7f247ab95207170b97ed5219840c00efff
HamZik <b1y9mn@gmail.com>
2023-02-10 09:14:26 +0900

    feat: 추가 안되는 오류 해결

--- section6/todo-training/src/components/Goals/Goals.js
+++ section6/todo-training/src/components/Goals/Goals.js
@@ -1,12 +1,9 @@
 const Goals = (props) => {
-  const handleDelete = (index) => {
-    props.onDelete(index)
-  }
 
   return (
     <div>
       {props.input.map((goal, index)=> (
-        <div key={index} onClick={handleDelete(index)}>{goal}</div>
+        <div key={index}>{goal}</div>
       ))}
     </div>
   );
