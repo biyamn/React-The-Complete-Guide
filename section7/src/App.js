8bbc9428c7a50f1970e54c01ef8f74033f1a7ebf
HamZik <b1y9mn@gmail.com>
2023-02-12 19:28:32 +0900

    style: input_invalid 수정

--- section7/src/App.js
+++ section7/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-
 import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
 import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
-import './App.css';
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
