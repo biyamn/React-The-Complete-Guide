d1e835423671bd275317df586050b55fc4f26531
HamZik <b1y9mn@gmail.com>
2023-03-05 21:16:44 +0900

    feat: modal test 생성

--- section8/section8-myOwn/src/App.js
+++ section8/section8-myOwn/src/App.js
@@ -2,18 +2,54 @@ import Contents from "./components/Contents";
 import Form from "./components/Form";
 import styles from './App.module.css';
 import { useState } from 'react';
+import Modal from 'react-modal'
 
 function App() {
   const [formData, setFormData] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const onSaveData = (data) => {
-    console.log(data);
     setFormData([...formData, data]);
-    console.log(formData);
   }
-  console.log(formData);
+  
+  const openModal = () => {
+    setIsModalOpen(true);
+  }
+
   return (
     <div className={styles.container}>
+      <button onClick={openModal}>modal test</button>
+      <Modal isOpen={isModalOpen} 
+        style={{
+          overlay: {
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(255, 255, 255, 0.75)'
+          },
+          content: {
+            position: 'absolute',
+            width: '200px',
+            height: '200px',
+            top: '40px',
+            left: '40px',
+            right: '40px',
+            bottom: '40px',
+            border: '1px solid #ccc',
+            background: '#fff',
+            overflow: 'auto',
+            WebkitOverflowScrolling: 'touch',
+            borderRadius: '4px',
+            outline: 'none',
+            padding: '20px'
+          }
+        }}
+        >
+        뭔가 잘못 입력함
+        <button onClick={()=> setIsModalOpen(false)}>취소</button>
+      </Modal>
       <Form onSaveData={onSaveData}/>
       <Contents data={formData} />
     </div>
