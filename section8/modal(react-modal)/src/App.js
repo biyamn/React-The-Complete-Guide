e773b94d2e924756d3614c22d8769d6c0e9ea70f
b1y9mn <b1y9mn@gmail.com>
2023-03-16 23:09:12 +0900

    feat: Portal 지우고 초기화

--- section8/modal(react-modal)/src/App.js
+++ section8/modal(react-modal)/src/App.js
@@ -1,55 +1,20 @@
-import Contents from "./components/Contents";
-import Form from "./components/Form";
-import styles from './App.module.css';
-import { useState } from 'react';
-import Modal from 'react-modal'
+import React, { useState } from 'react';
+import AddUser from '../src/components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
-  const [formData, setFormData] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [usersList, setUsersList] = useState([]);
 
-  const onSaveData = (data) => {
-    if (data.username === '') {
-      setIsModalOpen(true);
-    }
-    setFormData([...formData, data]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {id: Math.random().toString(), name: uName, age: uAge}]
+    })
   }
 
   return (
-    <div className={styles.container}>
-      <Modal isOpen={isModalOpen} 
-        style={{
-          overlay: {
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: 'rgba(255, 255, 255, 0.75)'
-          },
-          content: {
-            position: 'absolute',
-            width: '200px',
-            height: '200px',
-            top: '40px',
-            left: '40px',
-            right: '40px',
-            bottom: '40px',
-            border: '1px solid #ccc',
-            background: '#fff',
-            overflow: 'auto',
-            WebkitOverflowScrolling: 'touch',
-            borderRadius: '4px',
-            outline: 'none',
-            padding: '20px'
-          }
-        }}
-        >
-          뭔가 잘못 입력함
-        <button onClick={()=> setIsModalOpen(false)}>취소</button>
-      </Modal>
-      <Form onSaveData={onSaveData}/>
-      <Contents data={formData} />
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
---
+++ section8/modal(react-modal)/src/components/UI/ErrorModal.js
@@ -0,0 +1,44 @@
+import React, { ReactDOM } from 'react';
+import Card from './Card';
+import Button from './Button';
+import styles from './ErrorModal.module.css'
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onCLick={props.errorHandler} />;
+}
+
+const ModalOverlay = (props) => {
+  return (
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={props.errorHandler}>Okay</Button>
+        </footer>
+      </Card>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+  <>
+    {ReactDOM.createPortal(
+      <Backdrop errorHandler={props.errorHandler} />,
+      document.getElementById('backdrop-root')
+    )}
+    {ReactDOM.createPortal(
+      <ModalOverlay
+        title={props.title}
+        message={props.message}
+        errorHandler={props.errorHandler} 
+      />,
+      document.getElementById('overlay-root')
+    )}
+  </>
+)}
+
+export default ErrorModal;
