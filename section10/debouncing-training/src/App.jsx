d68a9be15bb7a99c61e0437dea2b3379e8b1538c
HamZik <b1y9mn@gmail.com>
2023-03-29 17:18:59 +0900

    feat: 디바운싱 구현

--- section10/debouncing-training/src/App.jsx
+++ section10/debouncing-training/src/App.jsx
@@ -1,30 +1,50 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [text, setText] = useState('');
+  console.log('App()이 리렌더링됩니다.')
+  const [text, setText] = useState("");
   const [inputIsValid, setInputIsValid] = useState(false);
 
   const textChangeHandler = (event) => {
     setText(event.target.value);
-    console.log(typeof(text));
-    console.log(text);
-    console.log(text.trim().length);
-    console.log(text.trim().length > 3);
-    // text가 아니라 event.target.value를 써야 하는 이유: event.target.value는 바로 바뀌는데 text는 App이 재렌더링되기 전까지 바뀌지 않아서(비동기라서)
-    setInputIsValid(event.target.value.trim().length > 9);
-  }
-  
+    // useEffect를 이 부분에 넣으면 안됨: https://legacy.reactjs.org/docs/hooks-rules.html
+  };
+
+  useEffect(() => {
+    console.log(`텍스트가 바뀌었어요! ${text}`)
+    // 이제 이거를 입력이 멈춘 뒤 500ms가 지났을 때만 실행해야 함
+    const timer = setTimeout(() => {
+      // text가 아니라 event.target.value를 써야 하는 이유: event.target.value는 바로 바뀌는데 text는 App이 재렌더링되기 전까지 바뀌지 않아서(비동기라서)
+      console.log("10글자 이상인지 검사합니다");
+      setInputIsValid(text.trim().length > 9);
+    }, 500);
+
+    // return 부분은 useEffect가 반환하는 값임. 
+    return () => {
+      console.log('텍스트가 바뀌어서 새 useEffect가 실행될 거래요! cleanup!')
+      clearTimeout(timer)
+    }
+    // [text]가 있는 이유: useEffect 안에 있는 함수가 원래 textChangeHandler 안에 있던 거임. textChangeHandler는 text가 바뀔 때 실행되는 거니까 [text]가 있는 게 맞음
+  }, [text]);
+
   const submitHandler = (event) => {
     event.preventDefault();
-  }
+  };
 
   return (
     <form onSubmit={submitHandler}>
-      <input type="text" value={text} onChange={textChangeHandler} placeholder="10글자 이상 입력해주세요" />
-      <button type="submit" disabled={!inputIsValid}>전송</button>
+      <input
+        type="text"
+        value={text}
+        onChange={textChangeHandler}
+        placeholder="10글자 이상 입력해주세요"
+      />
+      <button type="submit" disabled={!inputIsValid}>
+        전송
+      </button>
     </form>
-  )
+  );
 }
 
-export default App
+export default App;
