e90013859328761e029b49a4aa6bc34f79e91a5b
HamZik <b1y9mn@gmail.com>
2023-03-29 14:33:46 +0900

    chore: 기본 파일 세팅

--- section10/useEffect-training/src/App.jsx
+++ section10/useEffect-training/src/App.jsx
@@ -5,6 +5,22 @@ function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState('');
 
+  useEffect(() => {
+    console.log('App이 맨 처음 렌더링될 때 이게 보임');
+  }, []);
+
+  useEffect(() => {
+    console.log('App이 렌더링될 때마다 이게 보임');
+  });
+
+  useEffect(() => {
+    console.log('count가 변경될 때마다 이게 보임');
+  }, [count]);
+
+  useEffect(() => {
+    console.log('text가 변경될 때마다 이게 보임');
+  }, [text]);
+
   return (
     <div>
       <button onClick={() => setCount(count+1)}>{count}</button>
