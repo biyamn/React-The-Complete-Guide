80d1eb545adbe78aff308a84f441e3f7619a159e
b1y9mn <b1y9mn@gmail.com>
2023-04-04 11:28:01 +0900

    comment: createContext 관련 궁금증 메모

--- section10/context/src/store/auth-context.js
+++ section10/context/src/store/auth-context.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const AuthContext = React.createContext({
+  // App.js에서 AuthContext.Provider에 isLoggedIn과 onLogout을 쓸 건데 여기에 왜 중복으로 써주는지 궁금함. 
+  // 초기값인가??아닌데 여기에 false라고 하면 절대 안바뀐다고 본 것 같은데
   isLoggedIn: false,
   onLogout: () => {}
 });
