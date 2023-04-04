0ed0d98e4b33677da4edf9cb8bb3af3c2dbc8364
HamZik <b1y9mn@gmail.com>
2023-04-04 15:11:09 +0900

    feat: 모든 state를 context를 활용하여 auth-context.js에서 관리

--- section10/context-training/src/components/Login/Login.js
+++ section10/context-training/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 // 리듀서 함수는 컴포넌트 함수 바깥에 만든다. 컴포넌트 함수 내부에서 만들어진 어떤 데이터도 필요하지 않기 때문에 밖에서 만들어질 수 있다
 const emailReducer = (state, action) => {
@@ -51,6 +52,8 @@ const Login = (props) => {
     value: '',
     isValid: null,
   })
+
+  const authCxt = useContext(AuthContext);
 
   useEffect(() => {
     console.log('EFFECT RUNNING');
@@ -110,7 +113,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState);
+    authCxt.onLogin(emailState.value, passwordState);
   };
 
   return (
