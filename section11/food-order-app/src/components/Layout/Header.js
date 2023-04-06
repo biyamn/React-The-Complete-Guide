870cec5a626b81073c3ebe76719d1d96a92d6e78
b1y9mn <b1y9mn@gmail.com>
2023-04-07 08:56:26 +0900

    feat: HeaderCartButton 컴포넌트 추가

--- section11/food-order-app/src/components/Layout/Header.js
+++ section11/food-order-app/src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       {/* 하이픈(-)이 있으므로 .을 사용할 수 없어서 대괄호를 쓴다고 함: https://github.com/facebook/create-react-app/issues/11155*/}
       <div className={classes['main-image']}>
