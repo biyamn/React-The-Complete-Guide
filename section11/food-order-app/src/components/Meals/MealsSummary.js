4d364b40e71a862ae3523cdc742b1ec4adbc92f4
b1y9mn <b1y9mn@gmail.com>
2023-04-07 09:08:58 +0900

    feat: Meals 컴포넌트 추가

---
+++ section11/food-order-app/src/components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>맛있는 음식이 당신에게 배달됩니다</h2>
+      <p>다양한 기내식 중에서 마음에 드는 기내식을 선택하고 집에서 맛있는 점심 또는 저녁 식사를 즐겨보세요.</p>
+      <p>에어프랑스의 모든 기내식은 고품질의 식재료로 제시간에 조리되며 물론 숙련된 셰프가 제공합니다!</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
