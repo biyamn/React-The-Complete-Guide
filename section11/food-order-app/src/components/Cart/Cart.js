7c3b3f4428cb0055590d475641c913a9975cc91a
b1y9mn <b1y9mn@gmail.com>
2023-04-07 13:56:02 +0900

    feat: Cart 컴포넌트 추가

---
+++ section11/food-order-app/src/components/Cart/Cart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import classes from './Cart.module.css';
+
+// 장바구니 컴포넌트. 상단의 'Your Cart' 버튼을 클릭하면 모달로 이 장바구니를 렌더링 할 거임
+const Cart = () => {
+  const cartItems = <ul className={classes['cart-items']}>{[
+    {id: 'c1', name: 'Sushi', amount: 2, price: 12.99}
+  ].map(item => <li>{item.name}</li>)}</ul>;
+
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
