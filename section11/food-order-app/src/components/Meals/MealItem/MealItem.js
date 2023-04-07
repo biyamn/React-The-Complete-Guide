01a9ad578bdccb374d957350f91397de73b45fee
b1y9mn <b1y9mn@gmail.com>
2023-04-07 12:08:59 +0900

    fix: div => li

--- section11/food-order-app/src/components/Meals/MealItem/MealItem.js
+++ section11/food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,16 +1,23 @@
 import React from 'react';
+
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = (props) => {
   // 앞에 '$'를 붙여서 2자리까지 출력
   const price = `$${props.price.toFixed(2)}`
 
   return (
-    <div className={classes.meal}>
-      <h3>{props.name}</h3>
-      <div className={classes.description}>{props.description}</div>
-      <div className={classes.price}>{price}</div>
-    </div>
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm />
+      </div>
+    </li>
   );
 };
 
