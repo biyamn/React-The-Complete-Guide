e5ecc4dab5ad2e17475b075cd70ffc177c3813de
b1y9mn <b1y9mn@gmail.com>
2023-04-10 22:42:49 +0900

    feat: HeaderCartButton의 numberOfCartItems 생성

--- section11/food-order-app/src/components/Layout/HeaderCartButton.js
+++ section11/food-order-app/src/components/Layout/HeaderCartButton.js
@@ -6,7 +6,9 @@ import CartContext from '../../store/cart-context';
 const HeaderCartButton = props => {
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.length;
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
 
   return (
     <button className={classes.button} onClick={props.onClick}>
@@ -14,7 +16,7 @@ const HeaderCartButton = props => {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
