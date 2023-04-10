c311c489611bc9fa880bf9787703eb317a4fef4d
HamZik <b1y9mn@gmail.com>
2023-04-10 15:13:13 +0900

    feat: CartContetxt 추가

--- section11/food-order-app/src/App.js
+++ section11/food-order-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Header from "./components/Layout/Header";
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartContext from './store/cart-context';
 
 function App() {
   // Cart가 여기 있기 때문에 여기에 state를 만든다
@@ -16,7 +17,7 @@ function App() {
   }
 
   return (
-    <>
+    <CartContext>
       {/* 나는 Cart를 Modal 안의 Overlay 안에 넣었는데 강의에서는 Cart를 App 안의 Header 위에 넣었음. 
       즉 Cart(장바구니) 자체가 모달임. 'Your Cart'를 눌렀을 때 장바구니 구매 목록 모달이 나오므로 */}
       {cartIsShown && <Cart onClose={hideCartHandler} />}
@@ -24,7 +25,7 @@ function App() {
       <main>
         <Meals />
       </main>
-    </>
+    </CartContext>
   );
 }
 
--- section11/food-order-app/src/components/Layout/HeaderCartButton.js
+++ section11/food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = props => {
-  
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.length;
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
