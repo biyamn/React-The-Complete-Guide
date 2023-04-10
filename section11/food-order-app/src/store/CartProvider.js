998a53cb0552b07bffb527d69319a42018db9b82
b1y9mn <b1y9mn@gmail.com>
2023-04-10 23:13:42 +0900

    feat: cartReducer 추가

--- section11/food-order-app/src/store/CartProvider.js
+++ section11/food-order-app/src/store/CartProvider.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './cart-context';
 
-const CartProvider = props => {
-  const addItemToCartHandler = item => {
+// 음식 기준으로 수량을 관리하되 장바구니 항목의 가격을 모두 더한 것도 관리한다
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
 
+// 컴포넌트가 재랜더링될 때 cartReducer는 다시 평가되지 않음
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD') {
+
+  }
+
+  return defaultCartState;
+}
+
+const CartProvider = props => {
+
+  const [cartState, dispatchCartAction] =  useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = item => {
+    dispatchCartAction({type: 'ADD', item: item});
   };
 
   const removeItemFromCartHandler = id => {
-
+  dispatchCartAction({type: 'REMOVE', id: id});
   };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler
   }
