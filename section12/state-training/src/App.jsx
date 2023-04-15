ea103753f1c74f47ca031a81f36d8192cf1e5697
HamZik <b1y9mn@gmail.com>
2023-04-15 22:37:30 +0900

    feat: count 값 수정

--- section12/state-training/src/App.jsx
+++ section12/state-training/src/App.jsx
@@ -5,25 +5,25 @@ const App = () => {
   const [count, setCount] = useState(0);
 
   const plusHandler = () => {
-    // // 이렇게 하면 마지막 setCount만 적용되어 30씩 증가함
-    // setCount(count + 1); 
-    // setCount(count + 2);
-    // setCount(count + 30); 
+    // 이렇게 하면 마지막 setCount만 적용되어 30씩 증가함
+    setCount(count + 1); 
+    setCount(count + 2);
+    setCount(count + 3);
 
     // 이렇게 하면 모든 setCount가 적용되어 1+2+30, 총 33씩 증가함
-    setCount(count => count + 1); 
-    setCount(count => count + 2); 
-    setCount(count => count + 30); 
+    // setCount(count => count + 1); 
+    // setCount(count => count + 2);
+    // setCount(count => count + 3); 
   }
 
   const minusHandler = () => {
-    // setCount(count - 1); 
-    // setCount(count - 2); 
-    // setCount(count - 5); 
+    setCount(count - 1); 
+    setCount(count - 2); 
+    setCount(count - 3); 
 
-    setCount(count => count - 1); 
-    setCount(count => count - 2); 
-    setCount(count => count - 5); 
+    // setCount(count => count - 1); 
+    // setCount(count => count - 2); 
+    // setCount(count => count - 3); 
   }
 
   return (
