61fcfce2bd146bdd6b688c0331119b0b96658d06
HamZik <b1y9mn@gmail.com>
2023-04-15 21:53:09 +0900

    feat: setState 실험

---
+++ section12/state-training/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const plusHandler = () => {
+    // // 이렇게 하면 마지막 setCount만 적용되어 30씩 증가함
+    // setCount(count + 1); 
+    // setCount(count + 2);
+    // setCount(count + 30); 
+
+    // 이렇게 하면 모든 setCount가 적용되어 1+2+30, 총 33씩 증가함
+    setCount(count => count + 1); 
+    setCount(count => count + 2); 
+    setCount(count => count + 30); 
+  }
+
+  const minusHandler = () => {
+    // setCount(count - 1); 
+    // setCount(count - 2); 
+    // setCount(count - 5); 
+
+    setCount(count => count - 1); 
+    setCount(count => count - 2); 
+    setCount(count => count - 5); 
+  }
+
+  return (
+    <div className='container'>
+      <h2 className='int'>{ count }</h2>
+      <div>
+        <button className='plus' onClick={plusHandler}>+</button>
+        <button className='minus' onClick={minusHandler}>-</button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
