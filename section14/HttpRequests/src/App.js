48842418e7d4034ddb9192a2c10c015d7bad6ba8
b1y9mn <b1y9mn@gmail.com>
2023-05-09 13:19:56 +0900

    feat: firebase에 POST 요청 보내기

--- section14/HttpRequests/src/App.js
+++ section14/HttpRequests/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -8,8 +9,6 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  
-  
   // fetch는 promise객체(비동기)를 반환
   // async await를 사용하면 then을 사용하지 않아도 돼서 가독성이 높아짐
   const fetchMoviesHandler = useCallback(async () => {
@@ -17,21 +16,23 @@ function App() {
     setError(null);
     
     try {
-      const response = await fetch('https://swapi.dev/api/films/')
+      const response = await fetch('https://react-http-985cd-default-rtdb.firebaseio.com/movies.json')
       if (!response.ok) {
         throw new Error('Something went wrong!'); 
       }
       const data = await response.json();
       
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseData: movieData.release_data
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];  
+
+      for(const key in data) {
+        loadedMovies.push({
+          id: key, 
+          title: data[key].title, 
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate ,
+        })
+      }
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     } 
@@ -42,7 +43,20 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-http-985cd-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json(); 
+    console.log(data)
+  }
+
   let content = <p>Found no movies.</p>;
+  
   if (movies.length > 0) {  
     content = <MoviesList movies={movies} />
   }
@@ -54,6 +68,9 @@ function App() {
   }
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
